f43d2cd90944afa7852d191493cde3a662025ba3
alvianzf <alvianzf@gmail.com>
2025-11-23 19:40:28 +0700

    feat: make RouteLoader work in Route

--- Framework/Core/Router.php
+++ Framework/Core/Router.php
@@ -2,10 +2,36 @@
 
 namespace Framework\Core;
 
-class Router 
+class Router
 {
+    private array $routes;
+
+    public function __construct()
+    {
+        $loader = new RouteLoader();
+        $this->routes = $loader->load();
+    }
+
     public function dispatch()
     {
-        echo "NestPHP is running";
+        $method = $_SERVER['REQUEST_METHOD'];
+        $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+        foreach ($this->routes as $route) {
+            if ($route['method'] === $method && $route['path'] === $path) {
+                
+                $controller = new $route['class'];
+                $function = $route['function'];
+
+                $result = $controller->$function();
+
+                header('Content-Type: application/json');
+                echo json_encode($result);
+                return;
+            }
+        }
+
+        http_response_code(404);
+        echo json_encode(['error' => 'Not found']);
     }
 }
