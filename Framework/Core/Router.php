92faff6c61f0b917c08c61ffb006c3e31650eade
alvianzf <alvianzf@gmail.com>
2025-11-23 19:52:44 +0700

    feat: update dispatch loop to include RouteLoader changes

--- Framework/Core/Router.php
+++ Framework/Core/Router.php
@@ -4,34 +4,46 @@ namespace Framework\Core;
 
 class Router
 {
-    private array $routes;
+  private array $routes;
 
-    public function __construct()
-    {
-        $loader = new RouteLoader();
-        $this->routes = $loader->load();
-    }
+  public function __construct()
+  {
+    $loader = new RouteLoader();
+    $this->routes = $loader->load();
+  }
 
-    public function dispatch()
-    {
-        $method = $_SERVER['REQUEST_METHOD'];
-        $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+  public function dispatch()
+  {
+    $method = $_SERVER['REQUEST_METHOD'];
+    $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-        foreach ($this->routes as $route) {
-            if ($route['method'] === $method && $route['path'] === $path) {
-                
-                $controller = new $route['class'];
-                $function = $route['function'];
+    foreach ($this->routes as $route) {
 
-                $result = $controller->$function();
+      if ($route['method'] !== $method) {
+        continue;
+      }
 
-                header('Content-Type: application/json');
-                echo json_encode($result);
-                return;
-            }
+      if (preg_match($route['regex'], $path, $matches)) {
+
+        $controller = new $route['class'];
+        $function = $route['function'];
+
+        // Build param list in correct order
+        $args = [];
+        foreach ($route['params'] as $paramName) {
+          $args[] = $matches[$paramName];
         }
 
-        http_response_code(404);
-        echo json_encode(['error' => 'Not found']);
+        $result = $controller->$function(...$args);
+
+        header('Content-Type: application/json');
+        echo json_encode($result);
+        return;
+      }
     }
+
+
+    http_response_code(404);
+    echo json_encode(['error' => 'Not found']);
+  }
 }
