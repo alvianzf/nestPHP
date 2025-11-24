975f00924d0070c805041498036c1adbf37202e5
alvianzf <alvianzf@gmail.com>
2025-11-24 16:00:30 +0700

    feat: modify router to handle extra null output from base controller

--- Framework/Core/Router.php
+++ Framework/Core/Router.php
@@ -4,42 +4,44 @@ namespace Framework\Core;
 
 class Router
 {
-    private array $routes;
+  private array $routes;
 
-    public function __construct(private Container $container)
-    {
-        $loader = new RouteLoader($this->container);
-        $this->routes = $loader->load();
-    }
+  public function __construct(private Container $container)
+  {
+    $loader = new RouteLoader($this->container);
+    $this->routes = $loader->load();
+  }
 
-    public function dispatch()
-    {
-        $method = $_SERVER['REQUEST_METHOD'];
-        $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+  public function dispatch()
+  {
+    $method = $_SERVER['REQUEST_METHOD'];
+    $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-        foreach ($this->routes as $route) {
-            if ($route['method'] !== $method) continue;
+    foreach ($this->routes as $route) {
+      if ($route['method'] !== $method)
+        continue;
 
-            if (preg_match($route['regex'], $path, $matches)) {
-                $controller = $route['controller'];
-                $function = $route['function'];
+      if (preg_match($route['regex'], $path, $matches)) {
+        $controller = $route['controller'];
+        $function = $route['function'];
 
-                $args = [];
-                foreach ($route['params'] as $paramName) {
-                    if (isset($matches[$paramName])) $args[] = $matches[$paramName];
-                }
-
-                $result = $controller->$function(...$args);
-
-                if ($result !== null) {
-                    header('Content-Type: application/json');
-                    echo json_encode($result);
-                }
-                return;
-            }
+        $args = [];
+        foreach ($route['params'] as $paramName) {
+          if (isset($matches[$paramName]))
+            $args[] = $matches[$paramName];
         }
 
-        http_response_code(404);
-        echo json_encode(['error' => 'Not found']);
+        $result = $controller->$function(...$args);
+
+        if ($result !== null) {
+          header('Content-Type: application/json');
+          echo json_encode($result);
+        }
+        return;
+      }
     }
+
+    http_response_code(404);
+    echo json_encode(['error' => 'Not found']);
+  }
 }
